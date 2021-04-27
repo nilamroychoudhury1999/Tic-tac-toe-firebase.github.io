0f065d63565c378b118d935ac2d3fe3f561d2f3f
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2021-04-27 23:23:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+var box1 = document.getElementById("box1"),
+    box2 = document.getElementById("box2"),
+    box3 = document.getElementById("box3"),
+    box4 = document.getElementById("box4"),
+    box5 = document.getElementById("box5"),
+    box6 = document.getElementById("box6"),
+    box7 = document.getElementById("box7"),
+    box8 = document.getElementById("box8"),
+    box9 = document.getElementById("box9");
+
+// Your web app's Firebase configuration
+
+
+var database = firebase.database();
+
+var tictactoeState = database.ref('board/box1');
+
+const boardState = database.ref('board');
+
+boardState.on('value', (snapshot) => {
+    const data = snapshot.val();
+    // box1.innerHTML = data;
+    for (var boxName in data) {
+        if (boxName === 'turn') continue;
+        console.log(boxName);
+        document.getElementById(boxName).innerHTML = data[boxName];
+    }
+    getWinner();
+});
+
 var turn = document.getElementById("turn"),
 
     boxes = document.querySelectorAll("#main div"),
-    X_or_O = 0;
+    X_or_O = 'X';
+
+database.ref('board/turn').on('value', (snapshot) => {
+    const data = snapshot.val();
+    if (data) {
+        turn.innerHTML = data + " Turn Now";
+        X_or_O = data;
+    } else {
+        turn.innerHTML = "Play";
+    }
+});
 
 function selectWinnerBoxes(b1, b2, b3) {
     b1.classList.add("win");
@@ -12,17 +52,6 @@ function selectWinnerBoxes(b1, b2, b3) {
 }
 
 function getWinner() {
-
-    var box1 = document.getElementById("box1"),
-        box2 = document.getElementById("box2"),
-        box3 = document.getElementById("box3"),
-        box4 = document.getElementById("box4"),
-        box5 = document.getElementById("box5"),
-        box6 = document.getElementById("box6"),
-        box7 = document.getElementById("box7"),
-        box8 = document.getElementById("box8"),
-        box9 = document.getElementById("box9");
-
 
     if (box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML)
         selectWinnerBoxes(box1, box2, box3);
@@ -50,25 +79,29 @@ function getWinner() {
 
 }
 
-
-
 for (var i = 0; i < boxes.length; i++) {
     boxes[i].onclick = function() {
 
         if (this.innerHTML !== "X" && this.innerHTML !== "O") {
-            if (X_or_O % 2 === 0) {
+            if (X_or_O === "X") {
+                var value2set = "X";
+                database.ref('board/' + this.id).set(value2set);
+                database.ref('board/turn').set("O");
                 console.log(X_or_O);
-                this.innerHTML = "X";
-                turn.innerHTML = "O Turn Now";
+                this.innerHTML = value2set;
+                // turn.innerHTML = "O Turn Now";
                 getWinner();
-                X_or_O += 1;
+                // X_or_O += 1;
 
             } else {
+                var value2set = "O";
+                database.ref('board/' + this.id).set(value2set);
+                database.ref('board/turn').set("X");
                 console.log(X_or_O);
-                this.innerHTML = "O";
-                turn.innerHTML = "X Turn Now";
+                this.innerHTML = value2set;
+                // turn.innerHTML = "X Turn Now";
                 getWinner();
-                X_or_O += 1;
+                // X_or_O += 1;
             }
         }
 
@@ -82,7 +115,19 @@ function replay() {
         boxes[i].innerHTML = "";
         turn.innerHTML = "Play";
         turn.style.fontSize = "25px";
-
     }
+
+    database.ref('board').set({
+        box1: '',
+        box2: '',
+        box3: '',
+        box4: '',
+        box5: '',
+        box6: '',
+        box7: '',
+        box8: '',
+        box9: '',
+        turn: ''
+    });
 
 }
